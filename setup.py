7755892a7bb198b279b286b36dc813d301daad4a
Wish <100239673+wish13yt@users.noreply.github.com>
2024-10-30 17:17:09 -0500

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import requests
+import psutil
+print("[1] Install")
+print("[2] Info")
+print("[3] Exit")
+choice = input("What do ya think? (case sensitive, ex: Install) ")
+if (choice == "Install"):
+    print("Alright! We're installing...")
+    url = 'https://github.com/wish13yt/BitviewOS/BitviewOS.py?raw=true'
+
+
+    response = requests.get(url)
+    file_Path = 'BitviewOS.py'
+
+    if response.status_code == 200:
+        with open(file_Path, 'wb') as file:
+            file.write(response.content)
+        print('Installed!')
+    else:
+        print('Failed to download file')
+if (choice == "Info"):
+    print("Setup 1.0")
+    print("10/30/2024")
+    print("Idea by @thatgaminguser on PikiDiary")
+    print("Code by @wish on PikiDiary")
+if (choice == "Exit"):
+    raise SystemExit
